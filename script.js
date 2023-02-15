1df8671c6e23aa748e88e803aa7f4ea223c59973
andre <69599938+andrelumertz@users.noreply.github.com>
2023-02-15 01:09:36 -0300

    code js

---
+++ script.js
@@ -0,0 +1,34 @@
+// variables
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height');
+
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modalMessage = document.querySelector('.modal .title span');
+const modalBtnClose = document.querySelector('.modal button.close');
+
+
+
+form.onsubmit = function(event) {
+  event.preventDefault(); // função para evitar o padrao de recarregar a pagina
+  
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+
+  const result = IMC(weight, height)
+  const message = `Seu imc é de: ${result}`
+  
+  modalMessage.innerText = message
+  modalWrapper.classList.add('open')
+
+}
+
+// arrow function para fechar modal
+modalBtnClose.onclick = () => {
+    modalWrapper.classList.remove('open')
+}
+
+function IMC(weight, height) {
+    return ( weight / ((height / 100) ** 2)).toFixed(2);
+
+}

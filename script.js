1262cfa0c97cd851d7181a59b7da7657cb3b9bd8
andre <69599938+andrelumertz@users.noreply.github.com>
2023-02-16 18:15:33 -0300

    JJS refactoring

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
 
-const modalWrapper = document.querySelector('.modal-wrapper');
-const modalMessage = document.querySelector('.modal .title span');
-const modalBtnClose = document.querySelector('.modal button.close');
+//const modalWrapper = document.querySelector('.modal-wrapper');
+//const modalMessage = document.querySelector('.modal .title span');
+//const modalBtnClose = document.querySelector('.modal button.close');
 
+const Modal = {   //object literals
+  
+  //variables
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
 
+  open: function() { // function not shorthand
+    Modal.wrapper.classList.add("open");
+  },
+  close() {  //function with shorthand
+    Modal.wrapper.classList.remove("open");
+  }
+}
 
 form.onsubmit = function(event) {
   event.preventDefault(); // função para evitar o padrao de recarregar a pagina
@@ -18,14 +31,16 @@ form.onsubmit = function(event) {
   const result = IMC(weight, height)
   const message = `Seu imc é de: ${result}`
   
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  Modal.message.innerText = message
+  //modalWrapper.classList.add('open')
+  Modal.open()
 
 }
 
 // arrow function para fechar modal
-modalBtnClose.onclick = () => {
-    modalWrapper.classList.remove('open')
+Modal.buttonClose.onclick = () => {
+    //modalWrapper.classList.remove('open')
+    Modal.close()
 }
 
 function IMC(weight, height) {
